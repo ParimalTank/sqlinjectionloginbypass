5c6e25a3fb590978435082fd79b528c9674c8f53
ParimalTank <parimaltank2001@gmail.com>
2021-10-17 17:08:02 +0530

    new

--- db_config.php
+++ db_config.php
@@ -1,9 +1,23 @@
 <?php
     // Create connection
     $con = new mysqli("localhost","root","","loginsqli");
+?>
 
+<!-- 
+<?php
+const DB_HOST_NAME = 'localhost';
+const DB_NAME = 'security';
+const DB_USER_NAME = 'root';
+const DB_PASS = '';
+const TBL = 'users';
+
+class Database
+{
+  var $connection;
+
+  
     //To Prevent happening of sqlinjection
-    
+
         // const DB_HOST_NAME = 'localhost';
         // const DB_NAME = 'security';
         // const DB_USER_NAME = 'root';
@@ -19,4 +33,52 @@
   //     echo $e->getMessage();
   //   }
   // }
-?>
+
+  function __construct()
+  {
+    $this->connection = new mysqli(DB_HOST_NAME, DB_USER_NAME, DB_PASS, DB_NAME);
+  }
+
+  function getConnection()
+  {
+    return $this->connection;
+  }
+}
+
+$database = new Database();
+$conn = $database->getConnection();
+
+// function login($uname, $pass)
+// {
+//   global $conn;
+//   // $qry = 'SELECT * from ' . TBL . ' WHERE uname=:uname';
+//   // $qry = 'SELECT * from ' . TBL . " WHERE uname=" . $uname . " and password=" . $pass;
+//   $qry = "SELECT * FROM " . TBL . " WHERE uname = '$uname' AND password = '$pass'";
+//   echo $qry;
+//   $prep = $conn->prepare($qry);
+//   // $prep->execute([':uname' => $uname]);
+//   $prep->execute();
+//   $res = $prep->fetch(PDO::FETCH_ASSOC);
+
+//   if (!$res) return false;
+//   // return $res;
+//   if ($pass === $res['password']) {
+//     return true;
+//   }
+//   return false;
+// }
+
+function login($uname, $pass)
+{
+  global $conn;
+  $qry = "SELECT * FROM " . TBL . " WHERE uname = '$uname' AND password = '$pass'";
+  $res = mysqli_query($conn, $qry);
+  $check = mysqli_fetch_array($res);
+
+  if (isset($check)) {
+    return true;
+  }
+  return false;
+}
+
+?> -->
