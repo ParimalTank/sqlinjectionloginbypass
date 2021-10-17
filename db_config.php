33d9d8ea7b66c2e0b63a05684928d34c668535f8
ParimalTank <parimaltank2001@gmail.com>
2021-10-17 17:03:21 +0530

    Add

--- db_config.php
+++ db_config.php
@@ -1,4 +1,22 @@
 <?php
     // Create connection
     $con = new mysqli("localhost","root","","loginsqli");
+
+    //To Prevent happening of sqlinjection
+    
+        // const DB_HOST_NAME = 'localhost';
+        // const DB_NAME = 'security';
+        // const DB_USER_NAME = 'root';
+        // const DB_PASS = '';
+        // const TBL = 'users';
+    // function __construct()
+  // {
+  //   $dsn = "mysql:host=" . DB_HOST_NAME . ";dbname=" . DB_NAME;
+  //   try {
+  //     $this->connection = new PDO($dsn, DB_USER_NAME, DB_PASS);
+  //     $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //   } catch (PDOException $e) {
+  //     echo $e->getMessage();
+  //   }
+  // }
 ?>
